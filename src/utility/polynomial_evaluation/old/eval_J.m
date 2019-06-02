aeff5f157877c05041dcb493d659315f6d52653e
Shreyas Kousik <skousik@umich.edu>
2019-06-02 15:53:49 -0400

    added lots of polynomial eval stuff

--- src/utility/polynomial_evaluation/old/eval_J.m
+++ src/utility/polynomial_evaluation/old/eval_J.m
@@ -1,5 +1,5 @@
-function J_out = eval_J(k,Jcoeffs,Jpowers,N)
-% J_out = eval_J(k,Jcoeffs,Jpowers,N)
+function J_out = eval_J(k,J_coef,J_pows,N)
+% J_out = eval_J(k,J_coef,J_pows,N)
 %
 % Given a Jacobian matrix represented as a coefficients matrix and a powers
 % matrix (see diff_wk_wrt_k), evaluate the jacobian at k (Nk-by-1)
@@ -9,7 +9,7 @@ function J_out = eval_J(k,Jcoeffs,Jpowers,N)
     J_out = zeros(N,Nk) ;
  
     for idx = 1:Nk
-        J_out(:,idx) = Jcoeffs(1+N*(idx-1):N*idx,:) * ...
-            (prod(repmat(k',size(Jpowers,1),1).^Jpowers(:,(idx-1)*Nk+1:Nk*idx),2)) ;
+        J_out(:,idx) = J_coef(1+N*(idx-1):N*idx,:) * ...
+            (prod(repmat(k',size(J_pows,1),1).^J_pows(:,(idx-1)*Nk+1:Nk*idx),2)) ;
     end
 end
