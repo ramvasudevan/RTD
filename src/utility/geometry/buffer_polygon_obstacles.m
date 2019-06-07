e2de5402c63e9ca22948d11f9caac4b710a5df1b
Shreyas Kousik <skousik@umich.edu>
2019-06-06 22:16:17 -0400

    fixed the buffering for random polygonal obstacles

--- src/utility/geometry/buffer_polygon_obstacles.m
+++ src/utility/geometry/buffer_polygon_obstacles.m
@@ -2,9 +2,11 @@ function P_out = buffer_polygon_obstacles(P,b,miterlim)
 % Given obstacles P as a 2-by-N array of (x,y) points, buffer them by a
 % distance b and return a 2-by-(however many are needed) polygon array
 
-    % remove duplicate
+    % remove duplicate vertices
+    P = unique(P','rows','stable') ;
 
-    P = polyshape(P(1,:),P(2,:)) ;
+    % create polyshape
+    P = polyshape(P(:,1),P(:,2)) ;
     if nargin < 3
         P = polybuffer(P,b) ;
     elseif strcmp(miterlim,'square')
